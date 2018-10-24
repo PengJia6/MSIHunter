dd1ad98ef33ef6cfaf6951fa6ff486a54dd3d65b
PengJia6 <pengjia1110@gmial.com>
2018-10-24 10:00:32 +0800

    version20181023

--- src/MSIHunter.py
+++ src/MSIHunter.py
@@ -8,7 +8,10 @@
 #=============================================================================
 import pysam
 import os
-import pandas
+import pandas as pd
+
+MicroSatellite = pd.DataFrame()
+Distribution = {}
 def optInit():
     """
     argument procress
@@ -21,8 +24,8 @@ def optInit():
     parser.add_argument('-m','--Microsatellite',required=True,type=str,nargs=1,default="NA",help="path of the Microsatellite")
     parser.add_argument('-bed', '--bed_region', type=str, nargs=1, default=".NA", help="only procress read in this bed region")
     parser.add_argument('-t','--tumor_type',type=str,nargs=1,default="CRC",help="tumor type of the case(CRC,UCEC,STAD)")
-    parser.add_argument("-v1", "--verbose", help="increase output verbosity",
-                        action="store_true")
+    # parser.add_argument("-v1", "--verbose", help="increase output verbosity",
+    #                     action="store_true")
     # parser.add_argument("")
     args=parser.parse_args()
     arguments={}
@@ -30,24 +33,44 @@ def optInit():
     arguments["outPre"] = args.output_prefix[0]
     arguments["inputBed"] = args.bed_region[0]
     arguments["tumorType"]=args.tumor_type[0]
-    arguments["v"] = args.verbose
+    arguments["Microsatellite"] = args.Microsatellite[0]
+    # arguments["v"] = args.verbose
     # print("[MSIHunter I] input bam is ")
+    print("[MSIHunter INFO] Initializing...  Successfully!")
     return arguments
     # print(arguments)
     # print(args.tumor_type)
     # print(args.input_bam)
 def loadbed():
     print()
+def loadMicrosatellite(pathMicosatellite):
+    print("[MSIHunter INFO] Loading Microsatellite file from " + pathMicosatellite+" ...")
+    MicroSatellite=pd.read_table(pathMicosatellite,index_col=0)
+
+    print(MicroSatellite)
+    print("[MSIHunter INFO] Loading Microsatellite successfully")
 def bam2dis(bam):
+    print("[MSIHunter INFO] Loading bam file from "+bam+" ...")
+    print("[MSIHunter INFO] Loading bam file successfully")
+
+
+
+
+
 
 
     print()
 def main():
     import os
-
     argument=optInit() #argument procress
+    # global MicroSatellite,Distribution
     if argument["inputBed"]!=".":
         loadbed()
+    if os.path.exists(argument["Microsatellite"]):
+        loadMicrosatellite(argument["Microsatellite"])
+    else:
+        print('[MSIHunter ERROR] Fail to load Microsatellite from "'+argument["Microsatellite"]+' "')
+        return 2
     if os.path.exists(argument["inputBam"]):
         bam2dis(argument["inputBam"])
     else:
@@ -55,3 +78,5 @@ def main():
 if __name__ == "__main__":
 
     main()
+
+
